716e6767df6c707c720bc317aa29b8f446c952bb
Federico W <wuthrich09@gmail.com>
2022-08-06 03:23:12 -0300

    ADD: Home section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import '../src/components/NavBar/NavBar'
+import SaludoHome from './components/IntroHome/IntroHome';
 import NavBarLogo from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import 'bootswatch/dist/morph/bootstrap.min.css';
+import './Fonts/Fonts.css'
 
 
 
 function App() {
+
+
   return (
     
-          <div className='container-fluid flex bg-dark'>
+          <div  className='container-fluid flex bg-dark body'>
             <div id='NavBar'>
                 <NavBarLogo/> 
             </div>
+            <br/>
+            <SaludoHome/>
             <br></br>
-            <div id='Catalogo' className='container'>
+            <div  id='Catalogo' className='container'>
                 <ItemListContainer/>
             </div>
           </div>
