00b9b14da0fdf29a392348bc5b2bc54200312e90
Federico W <wuthrich09@gmail.com>
2022-08-18 21:55:41 -0300

    GHPAGES: NEW DEPLOY

--- src/components/Item/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/Item/ItemDetailContainer/ItemDetailContainer.js
@@ -1,3 +1,5 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { useState, useEffect } from "react";
 import ItemDetail from '../ItemDetail/ItemDetail';
